c7175e17602e95fbc0b28ebbcfdd9febeb7d0245
lebrancconvas <lebrancconvas@gmail.com>
2023-02-15 12:53:05 +0700

    ✨ feat: create api server

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	app := fiber.New();
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello API.");
+	})
 
+	app.Listen(":8012")
 }
